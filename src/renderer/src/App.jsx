83182ebf33765fc016e7c73e7047468f349a9e80
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-03-31 20:27:05 -0300

    style issues fixed.

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Settings from './components/Settings';
 import Translator from './components/Translator';
 import { LM_HARDCODED_LANGUAGES } from './utils/defaultValues';
--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,4 +1,4 @@
-import './assets/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
