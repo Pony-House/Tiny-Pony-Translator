f06da9f194f59c283273672339a6c88ab6cdc119
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-04-03 16:23:51 -0300

    file migration

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'electron-vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Ignore node_modules and build folders to save CPU/IO
+  ignored: ['**/node_modules/**', '**/dist/**'],
   main: {},
   preload: {},
   renderer: {
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('vite').UserConfig} */
-export default {
-  server: {
-    watch: {
-      // Ignore node_modules and build folders to save CPU/IO
-      ignored: ['**/node_modules/**', '**/dist/**'],
-    },
-  },
-};
